216b985487b0a5d7f941fca15003a3afa257e52a
Inchara Srinivasa <incs@iu.edu>
2025-03-17 02:20:12 -0400

    use tmp in vercel

--- src/lib/s3-server.ts
+++ src/lib/s3-server.ts
@@ -23,12 +23,13 @@ export async function downloadFromS3(file_key: string) {
         const obj = await s3.getObject(params).promise()
         // obj now has the actual downloaded file
 
-        const tempDir = path.join(process.cwd(), "tmp"); // Use process.cwd() instead of /tmp
-        if (!fs.existsSync(tempDir)) {
-            fs.mkdirSync(tempDir, { recursive: true });
-        }
+        // const tempDir = path.join(process.cwd(), "tmp"); // Use process.cwd() instead of /tmp
+        // if (!fs.existsSync(tempDir)) {
+        //     fs.mkdirSync(tempDir, { recursive: true });
+        // }
+        const file_name = `/tmp/inchara${Date.now().toString()}.pdf`; 
 
-        const file_name = path.join(tempDir, `pdf-${Date.now()}.pdf`);
+        // const file_name = path.join(tempDir, `pdf-${Date.now()}.pdf`);
         fs.writeFileSync(file_name, obj.Body as Buffer);
         return file_name;
     } catch (error) {

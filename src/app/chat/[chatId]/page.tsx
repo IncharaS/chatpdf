d184df0774441b3408edd82320df79451b2af79b
Inchara Srinivasa <incs@iu.edu>
2025-03-16 21:07:21 -0400

    add context to chat with AI, persistant logs with DB

--- src/app/chat/[chatId]/page.tsx
+++ src/app/chat/[chatId]/page.tsx
@@ -5,6 +5,8 @@ import { eq } from 'drizzle-orm';
 import { redirect } from 'next/navigation';
 import React from 'react'
 import ChatSideBar from '@/components/ChatSideBar';
+import PDFViewer from '@/components/PDFViewer';
+import ChatComponent from '@/components/ChatComponent';
 type Props = {
     params: {
         chatId: string;
@@ -26,6 +28,8 @@ const ChatPage = async ({ params }: Props) => {
     if (!_chats.find(chat => chat.id == parseInt(chatId))) {
         return redirect('/')
     }
+
+    const currentChat = _chats.find(chat => chat.id === parseInt(chatId))
     return (
         <div className='flex max-h-screen overflow-scroll'>
             <div className='flex w-full max-h-screen overflow-scroll'>
@@ -36,11 +40,11 @@ const ChatPage = async ({ params }: Props) => {
                 </div>
 
                 <div className='flex-[5] max-h-screen p-4 overflow-scroll'>
-                    {/* <PDFViewer />  */}
+                    <PDFViewer pdf_url={currentChat?.pdfUrl || ''} /> 
                 </div>
 
                 <div className='flex-[3] border-l-4 border-l-slate-200'>
-                    {/* <ChatComponent />  */}
+                    <ChatComponent chatId={parseInt(chatId)} /> 
                 </div>
             </div>
         </div>
